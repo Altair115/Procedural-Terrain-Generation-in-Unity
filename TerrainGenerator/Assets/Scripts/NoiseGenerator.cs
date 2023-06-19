bf3e75add5ffeb60357bd7c6390ff3540c2ca7fe
K.C <kevin.celinski@outlook.com>
2023-06-19 17:13:18 +0200

    :sparkles: Generating Terrain
    Implemented texture resolution for better looking textureMap and height map data is being applied to the mesh to generate a terrain

--- TerrainGenerator/Assets/Scripts/NoiseGenerator.cs
+++ TerrainGenerator/Assets/Scripts/NoiseGenerator.cs
@@ -8,17 +8,18 @@ public class NoiseGenerator : MonoBehaviour
     /// </summary>
     /// <param name="noiseSampleSize">how large of an area of the sample do you want to capture</param>
     /// <param name="scale">How "zoomed in" should the sample be higher value = more zoom</param>
+    /// <param name="resolution">how large the texture is going to be (a resolution of 1 will make each face of the mesh equal to 1 pixel)</param>
     /// <returns>a 2D float array</returns>
-    public static float[,] GenerateNoiseMap(int noiseSampleSize, float scale)
+    public static float[,] GenerateNoiseMap(int noiseSampleSize, float scale, int resolution = 1)
     {
-        float[,] noiseMap = new float[noiseSampleSize, noiseSampleSize];
+        float[,] noiseMap = new float[noiseSampleSize * resolution, noiseSampleSize * resolution];
 
-        for (int x = 0; x < noiseSampleSize; x++)
+        for (int x = 0; x < noiseSampleSize * resolution; x++)
         {
-            for (int y = 0; y < noiseSampleSize; y++)
+            for (int y = 0; y < noiseSampleSize * resolution; y++)
             {
-                float samplePosX = x / scale;
-                float samplePosY = y / scale;
+                float samplePosX = x / scale / resolution;
+                float samplePosY = y / scale / resolution;
 
                 noiseMap[x, y] = Mathf.PerlinNoise(samplePosX, samplePosY);
             }
--- TerrainGenerator/Assets/Scripts/TileGenerator.cs
+++ TerrainGenerator/Assets/Scripts/TileGenerator.cs
@@ -1,31 +1,57 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class TileGenerator : MonoBehaviour
 {
         [Header("Parameters")]
         public int noiseSampleSize;
         public float scale;
+        public float maxHeight = 1.0f;
+        public int textureResolution = 1;
 
-        private MeshRenderer _tileMeshrenderer;
+        private MeshRenderer _tileMeshRenderer;
         private MeshFilter _tileMeshFilter;
         private MeshCollider _tileMeshCollider;
 
         private void Start()
         {
             //get tile components
-            _tileMeshrenderer = GetComponent<MeshRenderer>();
+            _tileMeshRenderer = GetComponent<MeshRenderer>();
             _tileMeshFilter = GetComponent<MeshFilter>();
             _tileMeshCollider = GetComponent<MeshCollider>();
             
             GenerateTile(); 
         }
 
+        /// <summary>
+        /// generate a tile with a heightmap and a map texture
+        /// </summary>
         private void GenerateTile()
         {
             float[,] heightMap = NoiseGenerator.GenerateNoiseMap(noiseSampleSize, scale);
-            Texture2D heighMapTexture = TextureBuilder.BuildTexture(heightMap);
+            float[,] hdHeightMap = NoiseGenerator.GenerateNoiseMap(noiseSampleSize, scale, textureResolution);
 
-            _tileMeshrenderer.material.mainTexture = heighMapTexture;
+            Vector3[] verts = _tileMeshFilter.mesh.vertices;
+
+            for (int x = 0; x < noiseSampleSize; x++)
+            {
+                for (int z = 0; z < noiseSampleSize; z++)
+                {
+                    int index = (x * noiseSampleSize) + z;
+
+                    verts[index].y = heightMap[x, z] * maxHeight;
+                }
+            }
+
+            _tileMeshFilter.mesh.vertices = verts;
+            _tileMeshFilter.mesh.RecalculateBounds();
+            _tileMeshFilter.mesh.RecalculateNormals();
+
+            _tileMeshCollider.sharedMesh = _tileMeshFilter.mesh;
+            
+            Texture2D heighMapTexture = TextureBuilder.BuildTexture(hdHeightMap);
+
+            _tileMeshRenderer.material.mainTexture = heighMapTexture;
         }
 }
